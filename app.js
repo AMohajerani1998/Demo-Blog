e5455f8915d5634d7fabd3213238929eb1a525cc
AMohajerani1998 <ashkan.mohajerani1998@gmail.com>
2024-05-17 20:41:36 +0200

    Added user sign-up & sign-in operations with input validations Implemented session management and connected it to mongodb Implemented scrf token for security

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
 const express = require('express');
-
 const db = require('./data/database')
+const session = require('express-session')
+const mongodbStore = require('connect-mongodb-session')
+const mongoDBStore = mongodbStore(session)
 
-const postRoutes = require('./routes/posts')
+const csrf = require('csurf')
+
+const postRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -13,6 +17,24 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
+const sessionStore = new mongoDBStore({
+    uri: 'mongodb://localhost:27017',
+    databaseName: 'final-blog',
+    collection: 'sessions'
+})
+app.use(session({
+    secret: 'super-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+        maxAge: 30 * 24 * 60 * 68 * 1000,
+        sameSite: 'lax'
+    }
+}))
+
+app.use(csrf())
 
 app.use(postRoutes)
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,81 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const db = require("../data/database");
 
-router.get('/', function(req, res){
-    res.render('home')
-})
+router.get("/", function (req, res) {
+    res.render("home");
+});
+
+router.get("/sign-up", function (req, res) {
+    const csrfToken = req.csrfToken();
+    res.render("sign-up", {csrfToken:csrfToken});
+});
+
+router.post("/sign-up", async function (req, res) {
+    const userData = req.body;
+    const enteredEmail = userData.email;
+    const enteredConfirmEmail = userData["email-confirm"];
+    const eneteredPassword = userData.password;
+    if (
+        !eneteredPassword.length ||
+        !enteredEmail ||
+        !enteredConfirmEmail ||
+        !enteredEmail.includes("@") ||
+        enteredEmail !== enteredConfirmEmail
+    ) {
+        console.log("invalid inputs!");
+        return res.redirect("/sign-up");
+    }
+    const existingUser = await db
+        .getDb()
+        .collection("users")
+        .findOne({ email: enteredEmail });
+    if (existingUser) {
+        console.log("user already exists!");
+        return res.redirect("/sign-up");
+    }
+    hashedPassword = await bcrypt.hash(eneteredPassword, 12);
+    await db.getDb().collection("users").insertOne({
+        email: enteredEmail,
+        password: hashedPassword,
+    });
+    console.log("profile created");
+    res.redirect("/log-in");
+});
+
+router.get("/log-in", function (req, res) {
+    const csrfToken = req.csrfToken();
+    res.render("log-in", {csrfToken:csrfToken});
+});
+
+router.post("/log-in", async function (req, res) {
+    const userData = req.body;
+    const enteredEmail = userData.email;
+    const enteredPassword = userData.password;
+    const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail})
+    if (!existingUser){
+        console.log('user doesn\'t exist! ')
+        return res.redirect('/log-in')
+    }
+    const passwordCheck = await bcrypt.compare(enteredPassword, existingUser.password)
+    if (!passwordCheck){
+        console.log('password is wrong!')
+        return res.redirect('/log-in')
+    }
+    req.session.isAuthenticated = true;
+    req.session.user = {
+        id: existingUser._id,
+        email: enteredEmail
+    }
+    req.session.save(function(){
+        console.log('ok')
+        res.redirect('/new-post')
+    })
+});
+
+router.get("/new-post", function (req, res) {
+    res.render("new-post");
+});
 
 module.exports = router;
